db52120269b1e4f1c6f9082e09fc2bf42d2c4eaf
Jon P <jdperos@gmail.com>
2020-02-04 22:22:15 -0500

    Adding C# project for BFRES import

---
+++ BFRES Importer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BFRES_Importer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

2448e344e2ba57e2aae97e1fadde9a346bfa2859
Jon P <jdperos@gmail.com>
2020-03-05 18:25:31 -0500

    Writing some more bone animation data

--- BFRES Importer/FSKA/FSKA.cs
+++ BFRES Importer/FSKA/FSKA.cs
@@ -6,6 +6,8 @@ using System.Xml;
 using System.Linq;
 using ResU = Syroot.NintenTools.Bfres;
 using System.Diagnostics;
+using Toolbox.Library.Animations;
+using Bfres.Structs;
 
 namespace BFRES_Importer
 {
@@ -115,8 +117,76 @@ namespace BFRES_Importer
         {
             writer.WriteStartElement("BoneAnim");
             writer.WriteAttributeString("Name", boneAnim.Name);
+
+            // Flags 
+            writer.WriteAttributeString("UseScale", boneAnim.UseScale.ToString());
+            writer.WriteAttributeString("UseRotation", boneAnim.UseRotation.ToString());
+            writer.WriteAttributeString("UseTranslation", boneAnim.UseTranslation.ToString());
+
+            // writer.WriteAttributeString("FlagsCurve", boneAnim.FlagsCurve); // how do
+
+            writer.WriteAttributeString("ApplySegmentScaleCompensate", boneAnim.ApplySegmentScaleCompensate.ToString());
+            writer.WriteAttributeString("ApplyScaleUniform", boneAnim.ApplyScaleUniform.ToString());
+            writer.WriteAttributeString("ApplyScaleVolumeOne", boneAnim.ApplyScaleVolumeOne.ToString());
+            writer.WriteAttributeString("ApplyScaleOne", boneAnim.ApplyScaleOne.ToString());
+            writer.WriteAttributeString("ApplyRotateZero", boneAnim.ApplyRotateZero.ToString());
+            writer.WriteAttributeString("ApplyTranslateZero", boneAnim.ApplyTranslateZero.ToString());
+            writer.WriteAttributeString("ApplyRotateTranslateZero", boneAnim.ApplyRotateTranslateZero.ToString());
+            writer.WriteAttributeString("ApplyIdentity", boneAnim.ApplyIdentity.ToString());
+
+            writer.WriteAttributeString("BeginRotate", boneAnim.BeginRotate.ToString()); // Unknown purpose
+            writer.WriteAttributeString("BeginTranslate", boneAnim.BeginTranslate.ToString()); // Unknown purpose
+
+            // Base Data stores the initial state of bones in the animations
+            // writer.WriteAttributeString("BaseDataFlags", boneAnim.BaseData.Flags    .ToString()); // Unused
+            writer.WriteAttributeString("Scale", boneAnim.BaseData.Scale.ToString());
+            writer.WriteAttributeString("Rotate", boneAnim.BaseData.Rotate.ToString());
+            writer.WriteAttributeString("Translate", boneAnim.BaseData.Translate.ToString());
+            // writer.WriteAttributeString("BaseDataFlags", boneAnim.BaseData.Padding  .ToString()); // Unused
+
+            writer.WriteAttributeString("CurveCount", boneAnim.Curves.Count.ToString());
+
+            foreach (AnimCurve animCurve in boneAnim.Curves)
+            {
+                writer.WriteStartElement("Curve");
+                WriteBoneAnimCurve(writer, animCurve);
+                writer.WriteEndElement();
+            }
+
+
             writer.WriteEndElement();
         }
 
+        public static void WriteBoneAnimCurve(XmlWriter writer, AnimCurve animCurve)
+        {
+            // Flags
+            writer.WriteAttributeString("FrameType", animCurve.FrameType.ToString());
+            writer.WriteAttributeString("KeyType", animCurve.KeyType.ToString());
+            writer.WriteAttributeString("CurveType", animCurve.CurveType.ToString());
+
+            writer.WriteAttributeString("KeyCount", animCurve.Keys.Length.ToString());
+
+            writer.WriteAttributeString("StartFrame", animCurve.StartFrame.ToString());
+            writer.WriteAttributeString("EndFrame", animCurve.EndFrame.ToString());
+
+            writer.WriteAttributeString("DataScale", animCurve.Scale.ToString()); // multiplier to the raw key values to get the final result. Together with Data offset, it is chosen carefully to consider an optimal granularity between the stored values.
+            writer.WriteAttributeString("DataOffset", animCurve.Offset.ToString()); // added to the raw values (after multiplying them with Data scale) to get the final key value
+
+            writer.WriteAttributeString("DataDelta", animCurve.Delta.ToString()); // stores the difference between the first and last key value
+
+            for (int i = 0; i < animCurve.Keys.Length; i++)
+            {
+                writer.WriteStartElement("Key");
+
+                Animation.KeyGroup grp = new Animation.KeyGroup();
+
+                for (int j = 0; j < animCurve.Keys.Length; j++)
+                {
+
+                }
+
+                writer.WriteEndElement();
+            }
+        }
     }
 }

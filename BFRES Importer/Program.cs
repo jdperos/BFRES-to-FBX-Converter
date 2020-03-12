15fc95d3ad93070968293e8e3e828a2b8b288921
jdperos <jdperos@gmail.com>
2020-03-11 23:48:42 -0400

    Fixed a bug in writing mips caused from moving textures to their own folder

--- BFRES Importer/Program.cs
+++ BFRES Importer/Program.cs
@@ -17,7 +17,7 @@ namespace BFRES_Importer
         {
             if (args.Length == 0)
             {
-                FilePath = (AssetDir + "Npc_Gerudo_Queen.Tex1.bfres");
+                FilePath = (AssetDir + "Npc_Gerudo_Queen.Tex2.bfres");
                 OutputDir = "../../../../MedianDumps/";
             }
             else
@@ -70,9 +70,11 @@ namespace BFRES_Importer
                             jpTexture.SaveBitMap(OutputDir + "Mips/" + jpTexture.Name + i + ".tga", false, false, 0, i);
                         }
                     else
+                    {
                         if (!Directory.Exists(OutputDir + "Textures/"))
                             Directory.CreateDirectory(OutputDir + "Textures/");
-                    jpTexture.SaveBitMap(OutputDir + "Textures/" + jpTexture.Name + ".tga");
+                        jpTexture.SaveBitMap(OutputDir + "Textures/" + jpTexture.Name + ".tga");
+                    }
                     FTEX.WriteFTEXData(writer, res.Textures[ii]);
                 }
                 writer.WriteEndElement();
